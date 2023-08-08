9992d4a09d6453886b4f73e5cd9fe9f17bd219ea
Matt McCormick <matt.mccormick@kitware.com>
2023-08-08 12:51:09 -0400

    ENH: Read remaining metadata

--- src/itkOpenJPHImageIO.cxx
+++ src/itkOpenJPHImageIO.cxx
@@ -90,7 +90,6 @@ OpenJPHImageIO::ReadImageInformation()
   std::vector<uint8_t> & encodedBytes = this->m_Decoder->getEncodedBytes();
   this->ReadFile(this->m_FileName, encodedBytes);
 
-
   this->m_Decoder->readHeader();
 
   const auto size = this->m_Decoder->calculateSizeAtDecompositionLevel(this->m_DecompositionLevel);
@@ -136,109 +135,38 @@ OpenJPHImageIO::ReadImageInformation()
     this->SetPixelType(IOPixelEnum::VARIABLELENGTHVECTOR);
   }
 
+  const auto imageOffset = this->m_Decoder->getImageOffset();
+  this->SetOrigin(0, imageOffset.x);
+  this->SetOrigin(1, imageOffset.y);
+
+  MetaDataDictionary & thisDic = this->GetMetaDataDictionary();
+  // Number of wavelet decompositions
+  EncapsulateMetaData<int>(thisDic, "NumberOfDecompositions", this->m_Decoder->getNumDecompositions());
+  EncapsulateMetaData<bool>(thisDic, "IsReversible", this->m_Decoder->getIsReversible());
+  EncapsulateMetaData<bool>(thisDic, "ProgressionOrder", this->m_Decoder->getProgressionOrder());
+
+  std::vector<uint32_t> tileSize(2);
+  tileSize[0] = this->m_Decoder->getTileSize().width;
+  tileSize[1] = this->m_Decoder->getTileSize().height;
+  EncapsulateMetaData<decltype(tileSize)>(thisDic, "TileSize" , tileSize);
+
+  std::vector<uint32_t> tileOffset(2);
+  tileOffset[0] = this->m_Decoder->getTileOffset().x;
+  tileOffset[1] = this->m_Decoder->getTileOffset().y;
+  EncapsulateMetaData<decltype(tileOffset)>(thisDic, "TileOffset" , tileOffset);
+
+  std::vector<uint32_t> blockDimensions(2);
+  blockDimensions[0] = this->m_Decoder->getBlockDimensions().width;
+  blockDimensions[1] = this->m_Decoder->getBlockDimensions().height;
+  EncapsulateMetaData<decltype(blockDimensions)>(thisDic, "BlockDimensions" , blockDimensions);
+
+  const auto numberOfLayers = this->m_Decoder->getNumLayers();
+  EncapsulateMetaData<decltype(numberOfLayers)>(thisDic, "NumberOfLayers" , numberOfLayers);
+
+  const auto isUsingColorTransform = this-m_Decoder->getIsUsingColorTransform();
+  EncapsulateMetaData<decltype(isUsingColorTransform)>(thisDic, "IsUsingColorTransform" , isUsingColorTransform);
 }
 
-// void
-// OpenJPHImageIO::PopulateMetaDataDictionary()
-// {
-  // MetaDataDictionary & thisDic = this->GetMetaDataDictionary();
-  // EncapsulateMetaData<std::string>(thisDic, "Version", std::string(this->m_Version));
-  // EncapsulateMetaData<std::string>(thisDic, "PatientName", std::string(this->m_PatientName));
-  // EncapsulateMetaData<int>(thisDic, "PatientIndex", this->m_PatientIndex);
-  // EncapsulateMetaData<int>(thisDic, "ScannerID", this->m_ScannerID);
-  // EncapsulateMetaData<std::string>(thisDic, "CreationDate", std::string(this->m_CreationDate));
-  // EncapsulateMetaData<std::string>(thisDic, "ModificationDate", std::string(this->m_ModificationDate));
-  // EncapsulateMetaData<double>(thisDic, "SliceThickness", this->m_SliceThickness);
-  // EncapsulateMetaData<double>(thisDic, "SliceIncrement", this->m_SliceIncrement);
-  // std::vector<double> dataRange(2);
-  // dataRange[0] = this->m_DataRange[0];
-  // dataRange[1] = this->m_DataRange[1];
-  // EncapsulateMetaData<std::vector<double>>(thisDic, "DataRange", dataRange);
-  // EncapsulateMetaData<double>(thisDic, "MuScaling", this->m_MuScaling);
-  // EncapsulateMetaData<int>(thisDic, "NumberOfSamples", this->m_NumberOfSamples);
-  // EncapsulateMetaData<int>(thisDic, "NumberOfProjections", this->m_NumberOfProjections);
-  // EncapsulateMetaData<double>(thisDic, "ScanDistance", this->m_ScanDistance);
-  // EncapsulateMetaData<double>(thisDic, "SampleTime", this->m_SampleTime);
-  // EncapsulateMetaData<int>(thisDic, "ScannerType", this->m_ScannerType);
-  // EncapsulateMetaData<int>(thisDic, "MeasurementIndex", this->m_MeasurementIndex);
-  // EncapsulateMetaData<int>(thisDic, "Site", this->m_Site);
-  // EncapsulateMetaData<int>(thisDic, "ReconstructionAlg", this->m_ReconstructionAlg);
-  // EncapsulateMetaData<double>(thisDic, "ReferenceLine", this->m_ReferenceLine);
-  // EncapsulateMetaData<double>(thisDic, "Energy", this->m_Energy);
-  // EncapsulateMetaData<double>(thisDic, "Intensity", this->m_Intensity);
-  // EncapsulateMetaData<int>(thisDic, "RescaleType", this->m_RescaleType);
-  // EncapsulateMetaData<std::string>(thisDic, "RescaleUnits", std::string(this->m_RescaleUnits));
-  // EncapsulateMetaData<std::string>(thisDic, "CalibrationData", std::string(this->m_CalibrationData));
-  // EncapsulateMetaData<double>(thisDic, "RescaleSlope", this->m_RescaleSlope);
-  // EncapsulateMetaData<double>(thisDic, "RescaleIntercept", this->m_RescaleIntercept);
-  // EncapsulateMetaData<double>(thisDic, "MuWater", this->m_MuWater);
-// }
-
-// void
-// OpenJPHImageIO::SetHeaderFromMetaDataDictionary()
-// {
-  // MetaDataDictionary & metaData = this->GetMetaDataDictionary();
-
-  // std::string stringMeta;
-  // if (ExposeMetaData<std::string>(metaData, "Version", stringMeta))
-  // {
-  //   strncpy(this->m_Version, stringMeta.c_str(), 18);
-  // }
-  // if (ExposeMetaData<std::string>(metaData, "PatientName", stringMeta))
-  // {
-  //   strncpy(this->m_PatientName, stringMeta.c_str(), 42);
-  // }
-
-  // ExposeMetaData<int>(metaData, "PatientIndex", this->m_PatientIndex);
-  // ExposeMetaData<int>(metaData, "ScannerID", this->m_ScannerID);
-
-  // if (ExposeMetaData<std::string>(metaData, "CreationDate", stringMeta))
-  // {
-  //   strncpy(this->m_CreationDate, stringMeta.c_str(), 32);
-  // }
-  // if (ExposeMetaData<std::string>(metaData, "ModificationDate", stringMeta))
-  // {
-  //   strncpy(this->m_ModificationDate, stringMeta.c_str(), 32);
-  // }
-
-  // ExposeMetaData<double>(metaData, "SliceThickness", this->m_SliceThickness);
-  // ExposeMetaData<double>(metaData, "SliceIncrement", this->m_SliceIncrement);
-
-  // std::vector<double> dataRange(2);
-  // if (ExposeMetaData<std::vector<double>>(metaData, "DataRange", dataRange))
-  // {
-  //   this->m_DataRange[0] = dataRange[0];
-  //   this->m_DataRange[1] = dataRange[1];
-  // }
-
-  // ExposeMetaData<double>(metaData, "MuScaling", this->m_MuScaling);
-  // ExposeMetaData<int>(metaData, "NumberOfSamples", this->m_NumberOfSamples);
-  // ExposeMetaData<int>(metaData, "NumberOfProjections", this->m_NumberOfProjections);
-  // ExposeMetaData<double>(metaData, "ScanDistance", this->m_ScanDistance);
-  // ExposeMetaData<double>(metaData, "SampleTime", this->m_SampleTime);
-  // ExposeMetaData<int>(metaData, "ScannerType", this->m_ScannerType);
-  // ExposeMetaData<int>(metaData, "MeasurementIndex", this->m_MeasurementIndex);
-  // ExposeMetaData<int>(metaData, "Site", this->m_Site);
-  // ExposeMetaData<int>(metaData, "ReconstructionAlg", this->m_ReconstructionAlg);
-  // ExposeMetaData<double>(metaData, "ReferenceLine", this->m_ReferenceLine);
-  // ExposeMetaData<double>(metaData, "Energy", this->m_Energy);
-  // ExposeMetaData<double>(metaData, "Intensity", this->m_Intensity);
-  // ExposeMetaData<double>(metaData, "Intensity", this->m_Intensity);
-
-  // ExposeMetaData<int>(metaData, "RescaleType", this->m_RescaleType);
-  // if (ExposeMetaData<std::string>(metaData, "RescaleUnits", stringMeta))
-  // {
-  //   strncpy(this->m_RescaleUnits, stringMeta.c_str(), 18);
-  // }
-  // if (ExposeMetaData<std::string>(metaData, "CalibrationData", stringMeta))
-  // {
-  //   strncpy(this->m_CalibrationData, stringMeta.c_str(), 66);
-  // }
-
-  // ExposeMetaData<double>(metaData, "RescaleSlope", this->m_RescaleSlope);
-  // ExposeMetaData<double>(metaData, "RescaleIntercept", this->m_RescaleIntercept);
-  // ExposeMetaData<double>(metaData, "MuWater", this->m_MuWater);
-// }
 
 void
 OpenJPHImageIO::Read(void * buffer)
